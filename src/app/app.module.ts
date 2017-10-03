26b0b8ee38e9d8022382be269555f743d9784cc3
Andrew Ballinger <aballinger1981@gmail.com>
2017-10-03 16:00:29 -0400

    Added angular material and component modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { MatTableModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
