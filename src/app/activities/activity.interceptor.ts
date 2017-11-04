d89e2ef0a6dd6f1d3e66079b1b4d9a909ac2e745
Andrew Ballinger <aballinger1981@gmail.com>
2017-11-04 00:36:32 -0400

    added pace per mile conversion to returned split data for individual activities.

--- src/app/activities/activity.interceptor.ts
+++ src/app/activities/activity.interceptor.ts
@@ -11,15 +11,18 @@ export class ActivityInterceptor implements HttpInterceptor {
       if (event instanceof HttpResponse && event.body.length) {
         const duplicate = event.clone({
           body: event.body.map(activity => {
-            activity['average_pace'] = this.calculateAveragePace(activity);
+            activity['average_pace'] = this.calculateAveragePace(activity['moving_time'], activity['distance']);
             activity['distance'] = this.calculateMiles(activity);
             return activity;
           })
         });
       } else if (event instanceof HttpResponse) {
         const activity = event.body;
-        activity['average_pace'] = this.calculateAveragePace(activity);
+        activity['average_pace'] = this.calculateAveragePace(activity['moving_time'], activity['distance']);
         activity['distance'] = this.calculateMiles(activity);
+        activity['splits_standard'].map(split => {
+          return split['pace'] = this.calculateAveragePace(split['moving_time'], split['distance']);
+        });
         const duplicate = event.clone({
           body: activity
         });
@@ -27,11 +30,11 @@ export class ActivityInterceptor implements HttpInterceptor {
     });
   }
 
-  private calculateAveragePace(activity): string {
-    const movingTime = activity['moving_time'] / 60;
-    const distance: number = +(activity['distance'] / 1609.344).toFixed(2);
-    let averagePace = (movingTime / distance).toFixed(2).toString();
-    let seconds = Math.round(+(averagePace.substring(averagePace.length - 3)) * 60).toString();
+  private calculateAveragePace(movingTime, distance): string {
+    movingTime = movingTime / 60;
+    distance = +(distance / 1609.344).toFixed(2);
+    let averagePace: string = (movingTime / distance).toFixed(2).toString();
+    let seconds: string = Math.round(+(averagePace.substring(averagePace.length - 3)) * 60).toString();
     if (seconds.length === 1) { seconds = '' + seconds + '0'; }
     averagePace = `${averagePace.substring(0, 1)}:${seconds}`;
     return averagePace;
