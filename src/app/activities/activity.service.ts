a5fd96c069cf8abd0ff709d3ec446f526089a8c6
Andrew Ballinger <andrew@andrewballinger.com>
2017-11-21 15:50:51 -0500

    test commit

--- src/app/activities/activity.service.ts
+++ src/app/activities/activity.service.ts
@@ -40,7 +40,7 @@ export class ActivityService {
     const headers: HttpHeaders = new HttpHeaders()
     .set('Authorization', 'Bearer ' + localStorage.getItem('accessToken'));
     const response = this.http.get(url, { headers });
-    response.subscribe(data => console.log(data));
+    response.subscribe(data => console.log(data, 'bob'));
     return response;
   }
 
