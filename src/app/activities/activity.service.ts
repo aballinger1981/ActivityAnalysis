1f8e812919e4d7a791295f7773496b91aa86fd39
Andrew Ballinger <andrew@andrewballinger.com>
2017-11-22 15:49:32 -0500

    Continue setting up pagination

--- src/app/activities/activity.service.ts
+++ src/app/activities/activity.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { PageEvent } from '@angular/material';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 interface ActivityListData {
   startDate: string;
@@ -21,26 +22,29 @@ export class ActivityService {
   public activityList: Object;
   public athleteId: number;
   public activityTotal: number;
-  public dataChange: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  public pageEvent: PageEvent;
+  public pageIndex: number;
+  public pageSize: number;
+  public dataChange: BehaviorSubject<any> = new BehaviorSubject<any>([]);
   get data(): any[] { return this.dataChange.value; }
 
   constructor(
     private http: HttpClient
   ) { }
 
-  public getAthlete(): void {
+  public getAthlete(): Observable<any> {
+    if (this.athleteId) { return; }
     const url: string = 'https://www.strava.com/api/v3/athlete';
     const headers: HttpHeaders = new HttpHeaders()
       .set('Authorization', 'Bearer ' + localStorage.getItem('accessToken'));
     const response = this.http.get(url, { headers });
     response.subscribe(data => {
       this.athleteId = data['id'];
-      console.log(this.athleteId, data);
-      this.getActivityTotal();
     });
+    return response;
   }
 
-  public getActivityTotal(): void {
+  public getActivityTotal(): Observable<any> {
     const url: string = `https://www.strava.com/api/v3/athletes/${this.athleteId}/stats`;
     const headers: HttpHeaders = new HttpHeaders()
       .set('Authorization', 'Bearer ' + localStorage.getItem('accessToken'));
@@ -48,18 +52,25 @@ export class ActivityService {
     response.subscribe(data => {
       this.activityTotal = data['all_ride_totals']['count'] + data['all_run_totals']['count'] +
         data['all_swim_totals']['count'];
-      console.log(this.activityTotal);
     });
+    return response;
   }
 
-  public getActivities(): Observable<any> {
-    if (this.activityList) { return Observable.of(this.activityList); }
+  public getActivities(event?: PageEvent): Observable<any> {
+    console.log(event);
+    let index: number;
+    !event ? index = 1 : index = event.pageIndex + 1;
+    // if (this.activityList) { return Observable.of(this.activityList); }
+    !event ? this.pageSize = 10 : this.pageSize = event.pageSize;
+    !event ? this.pageIndex = 0 : this.pageIndex = event.pageIndex;
+    const url: string = 'https://www.strava.com/api/v3/athlete/activities';
     const headers: HttpHeaders = new HttpHeaders()
       .set('Authorization', 'Bearer ' + localStorage.getItem('accessToken'));
-    const response = this.http.get(`${this.activitiesUrl}?page=1`, { headers });
+    const response = this.http.get(`${url}?page=${index}&per_page=${this.pageSize}`, { headers });
     response.subscribe(data => {
       this.activityList = data;
     });
+    this.dataChange.next(response);
     return response;
   }
 
