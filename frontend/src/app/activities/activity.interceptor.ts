1913334d1463e3572700a0e3422d686a7c296471
Andrew Ballinger <aballinger1981@gmail.com>
2017-12-30 23:53:11 -0500

    added total time calculation

--- frontend/src/app/activities/activity.interceptor.ts
+++ frontend/src/app/activities/activity.interceptor.ts
@@ -41,33 +41,35 @@ export class ActivityInterceptor implements HttpInterceptor {
   private calculateAveragePace(movingTime, distance): string {
     movingTime = (movingTime / 60).toFixed(2);
     distance = +(distance / 1609.344).toFixed(2);
-    let averagePace: string = (movingTime / distance).toFixed(2);
-    let seconds: string = Math.round(+(averagePace.substring(averagePace.length - 3)) * 60).toString();
-    if (Number.parseInt(seconds, 10) < 10) {
-      seconds = '0' + seconds;
-    } else if (seconds.length === 1) { seconds = '' + seconds + '0'; }
-    const decimal: number = averagePace.indexOf('.');
-    averagePace = `${averagePace.substring(0, decimal)}:${seconds}`;
-    return averagePace;
+    let minutes: string = (movingTime / distance).toFixed(2);
+    let seconds: string = String(Math.round(+(minutes.substring(minutes.length - 3)) * 60));
+    return this.addZeros(minutes, seconds);
   }
 
-  private calculateTotalTime(movingTime): void {
-    let minutes: number = (movingTime / 60).toFixed(2);
-    if (minutes > 59.99) {
-      const hours: number = (minutes / 60).toFixed(2);
-      minutes = (60 * hours.substring(hours.length - 3)).toFixed(2);
-      const hoursDecimal: number = hours.indexOf('.');
+  private calculateTotalTime(movingTime: number): string {
+    let minutes: string = (movingTime / 60).toFixed(2);
+    if (+(minutes) > 59.99) {
+      const hours: string = (+(minutes) / 60).toFixed(2);
+      minutes = (60 * +(hours.substring(hours.length - 3))).toFixed(2);
+      const seconds: string = String(Math.floor(60 * +(minutes.substring(minutes.length - 3))));
+      return this.addZeros(minutes, seconds, hours);
     }
+    const seconds: string = String(Math.floor(60 * +(minutes.substring(minutes.length - 3))));
+    return this.addZeros(minutes, seconds);
+  }
+
+  private addZeros(minutes, seconds, hours?): string {
     const minutesDecimal: number = minutes.indexOf('.');
-    const seconds: number = (60 * minutes.substring(minutes.length - 3)).toFixed(2);
-    const secondsDecimal: number = seconds.indexOf('.');
     if (Number.parseInt(seconds, 10) < 10) {
       seconds = '0' + seconds;
-    } else if (seconds.length === 1) { seconds = '' + seconds + '0'; }
+    } else if (seconds.length === 1) {
+      seconds = '' + seconds + '0';
+    }
     if (hours) {
-      return `${hours.substring(0, hoursDecimal)}:${minutes.substring(0, minutesDecimal)}:${seconds.substring(0, secondsDecimal)}`;
+      const hoursDecimal: number = hours.indexOf('.');
+      return `${hours.substring(0, hoursDecimal)}:${minutes.substring(0, minutesDecimal)}:${seconds}`;
     } else {
-      return `${minutes.substring(0, minutesDecimal)}:${seconds.substring(0, secondsDecimal)}`;
+      return `${minutes.substring(0, minutesDecimal)}:${seconds}`;
     }
   }
 
